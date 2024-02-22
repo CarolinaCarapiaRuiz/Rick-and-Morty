817482b6989ecad22a783091d6e0a29a74eccaa3
CarolinaCarapiaRuiz <31716278+CaroCar@users.noreply.github.com>
2024-02-21 22:42:12 -0600

    Initial Commit

---
+++ Rick and Morty/Rick_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by Carolina Carapia on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
